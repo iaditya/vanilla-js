dd495940229a894472392e441cae7e719aef18d6
Aditya Gupta <iadityapansari@gmail.com>
2019-05-05 16:38:43 +0530

    completes Card C Fun

---
+++ card-games/README.md
@@ -0,0 +1,7 @@
+## card-games
+
+#### using Binding, prototypal inheritence, event listner, context of this and IIFE.
+
+#### Dynamic elements- created with JS. example = createElement, createDocumentFragment, etc.
+
+
--- card-games/main.js
+++ card-games/main.js
@@ -1,10 +1,10 @@
-;(function(window){
+; (function (window) {
 
 	//we will be building thee types of games- Flashcards, Solitaire, Hearts
 
 	// Game
 	// options -> based on game you play we pass the data, elem-> html elem
-	var Game = function(elem, option) {
+	var Game = function (elem, option) {
 		this.elem = document.getElementById(elem);
 		this.option = option;
 
@@ -27,33 +27,78 @@
 		this.elem.appendChild(this.deck_div);
 	}
 
-	window.Game = Game;  // Now Game construction function available in window object.
-	
 
 	// Deck
-	var Deck = function(deck_div, option) {
+	var Deck = function (deck_div, option) {
 		this.deck_data = option.data;
 
-		this.buildDeck = function() {
-			var parentFrag = document.createDocumentFragment();  // is node in memory not part of actual DOM.
+		this.buildDeck = function () {
+			//var abc = 123;
+			var parentFrag = document.createDocumentFragment();  // this node in memory not part of actual DOM.
 			deck_div.innerHTML = "";
-			for(var i= this.deck_data.length-1; i>=0; i--) {
+			for (var i = this.deck_data.length - 1; i >= 0; i--) {
 				var card = new Card();
+				console.log(this.deck_data[i]);
 				card.id = "card-" + i;
 				card.data = this.deck_data[i];
 				card.buildCard(parentFrag);
 			}
 			deck_div.appendChild(parentFrag);
 		}
-		
+
 	}
 
 
-	// Deck
-	var Card = function() {}
+	// Card
+	var Card = function () {
+		console.log(this);
+		this.id = "";
+		this.data = "";
+
+		this.cardCont = document.createElement("div");
+		this.cardCont.className = 'card_container';
+
+		this.cardFront = document.createElement("div");
+		this.cardFront.className = 'card_front';
+
+		this.cardBack = document.createElement("div");
+		this.cardBack.className = 'card_back';
+
+		this.buildCard = function (parentFrag) {
+			var flipDiv = document.createElement("div"),
+				frontValDiv = document.createElement("div"),
+				backValDiv = document.createElement("div"),
+				catDiv = document.createElement("div");
+			flipDiv.className = 'flip';
+			frontValDiv.className = 'front_val';
+			backValDiv.className = 'back_val';
+			catDiv.className = 'cat_val';
+
+			//add data
+			frontValDiv.innerHTML = this.data.q;
+			backValDiv.innerHTML = this.data.a;
+			catDiv.innerHTML = this.data.category;
+
+			this.cardFront.appendChild(frontValDiv);
+			this.cardFront.appendChild(catDiv);
+			this.cardBack.appendChild(backValDiv);
+
+			flipDiv.appendChild(this.cardFront);
+			flipDiv.appendChild(this.cardBack);
+
+			this.cardCont.id = this.id;
+			this.cardCont.appendChild(flipDiv);
+
+			//console.log(parentFrag);
+			//console.log(abc);
+			parentFrag.appendChild(this.cardCont);
+		}
+	}
 
 
-	// Deck
-	var DiscardPile = function() {}
+	// 
+	var DiscardPile = function () { }
+
+	window.Game = Game;  // Now Game construction function available in window object.
 
 })(window);
